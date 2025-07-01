5957340916bf584cb5e8a97fe9d3ac3b70c84584
Safaaiindia <safaaiindia@gmail.com>
2025-07-01 16:25:30 +0530

    Create firebase.js

---
+++ src/src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5DIhgHtx52-IQ83YNEZjo1RuvQ-NdGFY",
+  authDomain: "safaai-a4388.firebaseapp.com",
+  projectId: "safaai-a4388",
+  storageBucket: "safaai-a4388.appspot.com",
+  messagingSenderId: "937526139397",
+  appId: "1:937526139397:web:03e625b8bd8e9bf50bda6c",
+  measurementId: "G-DRC4HX9YL9"
+};
