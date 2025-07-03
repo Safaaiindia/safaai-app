727f10ae526b1890b724854f39a0c0073075d714
Safaaiindia <safaaiindia@gmail.com>
2025-07-03 16:16:23 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Safaai</h1>
+      {/* your routes or components go here */}
+    </div>
+  );
+}
+
+export default App;
